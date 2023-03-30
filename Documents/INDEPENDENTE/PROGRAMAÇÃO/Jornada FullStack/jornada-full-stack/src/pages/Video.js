0ebd2982d1281e58bb89a5aa4629300c8c596f00
emararipe <emararipe@gmail.com>
2023-03-30 18:06:50 -0300

    second commit - aula 2 - novo vídeo e cor de fundo

--- Documents/INDEPENDENTE/PROGRAMAÇÃO/Jornada FullStack/jornada-full-stack/src/pages/Video.js
+++ Documents/INDEPENDENTE/PROGRAMAÇÃO/Jornada FullStack/jornada-full-stack/src/pages/Video.js
@@ -1,35 +1,32 @@
-import React, {useRef, useState} from 'react'
-import './Video.css'
+import React, { useRef, useState } from "react";
+import "./Video.css";
 
 function Video() {
+  const videoRef = useRef(null);
+  const [play, setPlay] = useState(false);
 
-  const videoRef = useRef(null)
-  const [play, setPlay] = useState(false)
-
-    function handdleStart() {
-      if(!play) {
-        videoRef.current.play()
-        setPlay(true)
-      } 
-      else {
-        videoRef.current.pause()
-        setPlay(false)
-      }
-      
+  function handdleStart() {
+    if (!play) {
+      videoRef.current.play();
+      setPlay(true);
+    } else {
+      videoRef.current.pause();
+      setPlay(false);
     }
+  }
 
   return (
-    <div className='video'>
-      <video 
-        className='video__player'
+    <div className="video">
+      <video
+        className="video__player"
         //controls
         ref={videoRef}
         onClick={handdleStart}
-        loop 
-        src='https://firebasestorage.googleapis.com/v0/b/jornada-b5e9d.appspot.com/o/brecker2.mp4?alt=media&token=9aa979e9-0907-4093-abd5-a71efe4fc946'>
-      </video>
+        loop
+        src="https://assets.mixkit.co/videos/preview/mixkit-waves-in-the-water-1164-large.mp4"
+      ></video>
     </div>
-  )
+  );
 }
 
-export default Video
+export default Video;
