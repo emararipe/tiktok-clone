acc54a342f5c3a1e6a8ebe37c3eec9a431a89692
emararipe <emararipe@gmail.com>
2023-04-01 21:05:56 -0300

    aula 03 - parte 2

--- src/pages/components/footer/VideoFooter.js
+++ src/pages/components/footer/VideoFooter.js
@@ -1,4 +1,5 @@
 import React from "react"; //feito com o snippet rfce
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
 import "./VideoFooter.css";
 
 function VideoFooter() {
@@ -6,10 +7,14 @@ function VideoFooter() {
     <div className="videoFooter">
       <div className="videoFooter__text">
         <h3>@Manu</h3>
-        <p>Descrição do vídeo</p>
+        <p>
+          Eu danço a dança das tuas marés.
+        </p>
         <div className="videoFooter__music">
-          <p>Ícone</p>
-          <p>Título da música - banda</p>
+          <MusicNoteIcon className="videoFooter__icon" />
+          <div className="videoMusicFooter__text">
+            <p>Bom Mesmo É Estar Debaixo... - Luedji Luna</p>
+          </div>
         </div>
       </div>
       <img
