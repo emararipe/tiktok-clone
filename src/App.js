000d31d2e83c985347cfa2808bf06b37a4b82623
emararipe <emararipe@gmail.com>
2023-04-04 10:52:34 -0300

    Aula 06 - responsividade mobile

--- src/App.js
+++ src/App.js
@@ -5,36 +5,42 @@ import db from "./config/firebase";
 import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
 
 function App() {
+
+  let maxHeight;
+  if(window.innerHeight <= 800){
+    maxHeight = window.innerHeight
+  }
+
   const [video, setVideos] = useState([]);
 
   async function getVideos() {
     const videosCollection = collection(db, "videos");
     const videosSnapshot = await getDocs(videosCollection);
     const videosList = videosSnapshot.docs.map((doc) => doc.data());
-    setVideos(videosList)
+    setVideos(videosList);
   }
 
-  useEffect(() => { /* usando useEffect os dados dos vídeos, caso haja modificação, é substituído por inteiro, não somente a parte modificada[?] */
+  useEffect(() => {
+    /* usando useEffect os dados dos vídeos, caso haja modificação, é substituído por inteiro, não somente a parte modificada[?] */
     getVideos();
   }, []);
 
   return (
-    <div className="App">
+    <div className="App" style={{maxHeight: maxHeight + "px"}}>
       <div className="app__videos">
         {video.map((item) => {
           return (
             <Video
+              description={item.description}
               likes={item.likes}
               messages={item.messages}
-              shares={item.shares}
-              name={item.name}
-              description={item.description}
               music={item.music}
+              name={item.name}
+              shares={item.shares}
               url={item.url}
             />
           );
         })}
-
       </div>
     </div>
   );
