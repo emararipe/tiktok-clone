6427115a3de2e4c5be6e68ff9a8fb1d64d2b193c
emararipe <emararipe@gmail.com>
2023-04-03 21:57:25 -0300

    Aula 05 - limpeza de código

--- src/App.js
+++ src/App.js
@@ -35,15 +35,6 @@ function App() {
           );
         })}
 
-        {/* <Video
-          likes={100}
-          messages={200}
-          shares={300}
-          name="Manu"
-          description="Eu danço a dança das tuas marés."
-          music="Bom Mesmo É Estar Debaixo... - Luedji Luna"
-          url="https://assets.mixkit.co/videos/preview/mixkit-waves-in-the-water-1164-large.mp4"
-        /> */}
       </div>
     </div>
   );
